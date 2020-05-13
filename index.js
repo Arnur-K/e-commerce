f7e62e38e5447ee6fd1dede33a6739140395f11c
Arnur Kupanov <ncase01@gmail.com>
2020-05-14 04:14:01 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send(`
+      <div>
+        <form method="POST">
+            <input type="email" name="email" placeholder="email" />
+            <input type="password" name="password" placeholder="password" />
+            <input type="password" name="passwordConfirmation" placeholder="confirm password" />
+            <button>Sign up</button>
+        </form>
+      </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('WOO');
+});
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
